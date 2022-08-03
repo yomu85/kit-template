3aa1af2095a0e1dfeea173fe673066b3f5d73562
medicx <medicx@alltmro.com>
2022-08-03 13:33:22 +0900

    create project

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<h1 class="text-3l font-bold underlne">
+  Hello world!
+</h1>
+
+<button class="btn btn-outline">Button</button>
+<button class="btn btn-outline btn-primary">Button</button>
+<button class="btn btn-outline btn-secondary">Button</button>
+<button class="btn btn-outline btn-accent">Button</button>
+
+<h2>전 제목이예요 <span>전 스팬안이예요</span></h2>
+
+<style lang="scss">
+  h2 {
+    font-size: 2rem;
+    display: flex;
+    box-shadow: 1px 1px 1px 1px #3B9DFF;
+    span {
+      font-size: 1rem;
+    }
+  }
+</style>
